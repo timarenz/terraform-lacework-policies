05a4caace3f0abfbf6a6f2beaa7ddf93e79893fc
Tim Arenz <tim@arenz.cc>
2022-03-07 16:36:14 +0100

    Adding support for YAML-based policies

---
+++ examples/local-json-content/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    lacework = {
+      source  = "lacework/lacework"
+      version = "0.16.0"
+    }
+  }
+}
+
+provider "lacework" {}
+
+module "lacework-policies-as-code" {
+  # source                = "git::https://github.com/timarenz/terraform-lacework-policies.git"
+  source                = "../../"
+  lacework_content_path = "${path.root}/content"
+  format                = "json"
+}
